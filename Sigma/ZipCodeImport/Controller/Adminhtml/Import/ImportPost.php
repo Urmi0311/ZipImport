36b4326c378862d4947f389036cc1e5e839ef0e0
Urmi0311 <urmi.kothari@sigmainfo.net>
2024-06-03 09:49:52 +0530

    warning solved

--- Sigma/ZipCodeImport/Controller/Adminhtml/Import/ImportPost.php
+++ Sigma/ZipCodeImport/Controller/Adminhtml/Import/ImportPost.php
@@ -17,18 +17,18 @@ class ImportPost extends \Sigma\ZipCodeImport\Controller\Adminhtml\Import
     public function execute()
     {
         $request = $this->getRequest();
-            if ($request->isPost() && !empty($request->getFiles('import_zipcode_file'))) {
-                try {
+        if ($request->isPost() && !empty($request->getFiles('import_zipcode_file'))) {
+            try {
                 $importHandler = $this->_objectManager->create(CsvImportHandler::class);
                 $importHandler->importFromCsvFile($this->getRequest()->getFiles('import_zipcode_file'));
                 $this->messageManager->addSuccess(__('Zip Code import has been successfully completed.'));
             } catch (\Magento\Framework\Exception\LocalizedException $e) {
                 $errorFile = $this->_objectManager->get(DirectoryList::class)
-                       ->getPath(DirectoryList::VAR_DIR) . '/import_zipcode_errors.csv';
+                   ->getPath(DirectoryList::VAR_DIR) . '/import_zipcode_errors.csv';
                 $downloadLink = $this->_objectManager->get(UrlInterface::class)
-                   ->getUrl('zipcodeimport/import/downloadErrorCsv', ['file' => 'import_zipcode_errors.csv']);
+                ->getUrl('zipcodeimport/import/downloadErrorCsv', ['file' => 'import_zipcode_errors.csv']);
                 $this->messageManager->addError($e->getMessage() .
-                   ' <a href="' . $downloadLink . '">Download full report</a>');
+                ' <a href="' . $downloadLink . '">Download full report</a>');
             } catch (\Exception $e) {
                 $this->messageManager->addError(__($e->getMessage()));
             }
